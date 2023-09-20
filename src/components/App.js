d00557ced3e24a708c86604b28bb62745b45cd60
Bhumi <bhumione9545@gmail.com>
2023-09-20 21:39:22 +0530

    Attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,10 @@ const App = () => {
   return (
     <div id="main">
                {/* Do not remove the main div */}
+               <ol key={relativeList}>
+                <li key={relativeListItem1}>Ramesh</li>
+                <li key={relativeListItem2}>Sukeshs</li>
+               </ol>
     </div>
   )
 }
